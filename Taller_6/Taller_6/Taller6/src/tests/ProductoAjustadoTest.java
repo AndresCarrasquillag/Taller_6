513dd988c0b60021cdf4e5e3d5f9c791ee4b3d1f
AndresCarrasquillag <111008783+AndresCarrasquillag@users.noreply.github.com>
2023-05-22 02:02:14 -0500

    Update ProductoAjustadoTest.java

--- Taller_6/Taller_6/Taller6/src/tests/ProductoAjustadoTest.java
+++ Taller_6/Taller_6/Taller6/src/tests/ProductoAjustadoTest.java
@@ -17,8 +17,11 @@ class ProductoAjustadoTest {
 		Ingrediente ing = new Ingrediente("ingrediente", 12000);
 		
 		ProductoAjustado productoAjustado = new ProductoAjustado(producto, ing, "con");
+		ProductoAjustado productoAjustado2 = new ProductoAjustado(producto, ing, "sin");
 		assertEquals("producto con ingrediente", productoAjustado.getNombre());
 		assertEquals(24000, productoAjustado.getPrecio());
+		assertEquals("producto sin ingrediente", productoAjustado2.getNombre());
+		assertEquals(12000, productoAjustado2.getPrecio());
 		
 	}
 
