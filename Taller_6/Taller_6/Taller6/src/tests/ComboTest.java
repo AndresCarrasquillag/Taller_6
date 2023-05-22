23b28c1c8744b7fa2d698d989d6d8f99155cb177
AndresCarrasquillag <111008783+AndresCarrasquillag@users.noreply.github.com>
2023-05-22 02:01:05 -0500

    Update ComboTest.java

--- Taller_6/Taller_6/Taller6/src/tests/ComboTest.java
+++ Taller_6/Taller_6/Taller6/src/tests/ComboTest.java
@@ -4,11 +4,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import modelo.Combo;
+import modelo.ProductoMenu;
+
 class ComboTest {
 
 	@Test
 	void test() {
-		;
+		ProductoMenu producto1 = new ProductoMenu("prod1", 1200);
+		ProductoMenu producto2 = new ProductoMenu("prod2", 1300);
+		ProductoMenu producto3 = new ProductoMenu("prod3", 1400);
+		Combo combo = new Combo("combo", 0.1, producto1, producto2, producto3);
+		assertEquals("combo", combo.getNombre());
+		assertEquals((0.9*1200)+(0.9*1300)+(0.9*1400), combo.getPrecio());
+		
 	}
 
 }
